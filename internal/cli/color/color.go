d0beedfc03f575b68a66b3b4d2df9f639fd1f1cf
reugn <reugpro@gmail.com>
2024-09-11 22:09:09 +0300

    refactor: make the cli package internal

---
+++ internal/cli/color/color.go
@@ -0,0 +1,55 @@
+package color
+
+import "fmt"
+
+const (
+	reset   = "\033[0m"
+	red     = "\033[31m"
+	green   = "\033[32m"
+	yellow  = "\033[33m"
+	blue    = "\033[34m"
+	magenta = "\033[35m"
+	cyan    = "\033[36m"
+	gray    = "\033[37m"
+	white   = "\033[97m"
+)
+
+// Red adds red color to str in terminal.
+func Red(str string) string {
+	return fmt.Sprintf("%s%s%s", red, str, reset)
+}
+
+// Green adds green color to str in terminal.
+func Green(str string) string {
+	return fmt.Sprintf("%s%s%s", green, str, reset)
+}
+
+// Yellow adds yellow color to str in terminal.
+func Yellow(str string) string {
+	return fmt.Sprintf("%s%s%s", yellow, str, reset)
+}
+
+// Blue adds blue color to str in terminal.
+func Blue(str string) string {
+	return fmt.Sprintf("%s%s%s", blue, str, reset)
+}
+
+// Magenta adds magenta color to str in terminal.
+func Magenta(str string) string {
+	return fmt.Sprintf("%s%s%s", magenta, str, reset)
+}
+
+// Cyan adds cyan color to str in terminal.
+func Cyan(str string) string {
+	return fmt.Sprintf("%s%s%s", cyan, str, reset)
+}
+
+// Gray adds gray color to str in terminal.
+func Gray(str string) string {
+	return fmt.Sprintf("%s%s%s", gray, str, reset)
+}
+
+// White adds white color to str in terminal.
+func White(str string) string {
+	return fmt.Sprintf("%s%s%s", white, str, reset)
+}
---
+++ internal/cli/spinner.go
@@ -0,0 +1,69 @@
+package cli
+
+import (
+	"bufio"
+	"fmt"
+	"time"
+)
+
+const (
+	moveCursorBackward  = "\033[%dD"
+	clearLineFromCursor = "\033[K"
+	progressRune        = '.'
+)
+
+type spinner struct {
+	length   int
+	interval time.Duration
+	writer   *bufio.Writer
+	signal   chan struct{}
+}
+
+func newSpinner(length int, interval time.Duration, writer *bufio.Writer) *spinner {
+	return &spinner{
+		length:   length,
+		interval: interval,
+		writer:   writer,
+		signal:   make(chan struct{}),
+	}
+}
+
+//nolint:errcheck
+func (s *spinner) start() {
+	go func() {
+		ticker := time.NewTicker(s.interval)
+		defer ticker.Stop()
+		var n int
+		for {
+			select {
+			case <-s.signal:
+				if n > 0 {
+					s.clear(n)
+				}
+				s.signal <- struct{}{}
+				return
+			case <-ticker.C:
+				if n < s.length {
+					s.writer.WriteRune(progressRune)
+					s.writer.Flush()
+					n++
+				} else {
+					s.clear(n)
+					n = 0
+				}
+			}
+		}
+	}()
+}
+
+//nolint:errcheck
+func (s *spinner) clear(n int) {
+	s.writer.WriteString(fmt.Sprintf(moveCursorBackward, n))
+	s.writer.WriteString(clearLineFromCursor)
+	s.writer.Flush()
+}
+
+func (s *spinner) stop() {
+	s.signal <- struct{}{}
+	<-s.signal
+}
